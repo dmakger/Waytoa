91181f6a8a498510daa9e3b00918e1fca5788c24
dmakger <dmakger@yandex.ru>
2023-03-07 11:48:22 +0300

    Сделал [B. Змейка-1]. Вложенные циклы. Дополнение. Informatics: Python Pro

---
+++ 9. вложенные циклы. Дополнение/B.py
@@ -0,0 +1,45 @@
+# Задача №112375. Змейка-1
+# Напишите программу, которая заполняет матрицу из N строк и M столбцов натуральными числами
+# змейкой, как показано в примере.
+#
+# Входные данные
+# Входная строка содержит числа N и M ( 1 ≤ N , M ≤ 100 ), разделённые пробелом.
+#
+# Выходные данные
+# Программа должна вывести матрицу, заполненную заданным способом.
+
+def get_matrix(size: int):
+    return [[] for _ in range(size)]
+
+
+def fill_snake_matrix(matrix: list, n: int, m: int):
+    for i in range(n):
+        mas = [x for x in range(m * i + 1, m * i + m + 1)]
+        if i % 2:
+            mas = mas[::-1]
+        matrix[i] = mas
+
+
+def get_count_space(rows: int, cols: int):
+    r = len(str(rows * cols)) + 1
+    if r < 4:
+        return 4
+    return r
+
+
+def print_matrix(matrix: list, count_space: int):
+    for line in matrix:
+        for el in line:
+            print(f"{' ' * (count_space - len(str(el)))}{el}", end='')
+        print()
+
+
+def main():
+    n, m = map(int, input().split())
+    matrix = get_matrix(n)
+    fill_snake_matrix(matrix, n, m)
+    print_matrix(matrix, get_count_space(n, m))
+
+
+if __name__ == '__main__':
+    main()
